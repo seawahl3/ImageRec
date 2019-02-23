d005136dd2454f3a5a48701bdb68f685ce2d6bf8
jasonfagerberg-toast <jason.fagerberg@toasttab.com>
2019-02-23 13:22:14 -0500

    speech recogn v1

--- SpeechRecog/speech.py
+++ SpeechRecog/speech.py
@@ -1,14 +1,26 @@
 import json
-import librosa    
+import librosa
+import moviepy.editor as mp 
 
 from watson_developer_cloud import SpeechToTextV1
 
-stt = SpeechToTextV1(    iam_apikey='t0rpKCce8HfPqMie-2bz3QPi301_0Dj5Nfc-ypuxsb1m', url='https://gateway-wdc.watsonplatform.net/speech-to-text/api')
-audio_file = open("IMG_5623.flac", "rb")
+class SpeechRecog ():
+    def __init__(self):
+        pass
+
+    def get_audio_transcription(self, video_file): 
+        stt = SpeechToTextV1(    iam_apikey='t0rpKCce8HfPqMie-2bz3QPi301_0Dj5Nfc-ypuxsb1m', url='https://gateway-wdc.watsonplatform.net/speech-to-text/api')
+        clip = mp.VideoFileClip(video_file)
+        clip.audio.write_audiofile("theaudio.mp3")
+        audio_file = open("theaudio.mp3", "rb")
 
 
-with open('transcript_result.json', 'w') as fp:
-    result = stt.recognize(audio_file, content_type="audio/x-flac",
-                           continuous=True, timestamps=True,
-                           max_alternatives=1)
-    json.dump(result.get_result(), fp, indent=2)
+        with open('transcript_result.json', 'w') as fp:
+            result = stt.recognize(audio_file, content_type="audio/mp3",
+                                continuous=True, timestamps=True,
+                                max_alternatives=1)
+            json.dump(result.get_result(), fp, indent=2)
+            audioTranscription = []
+            for transcription in result.get_result()["results"][0]["alternatives"][0]["timestamps"]:
+                audioTranscription.append((transcription[0],transcription[1]))
+            return audioTranscription
